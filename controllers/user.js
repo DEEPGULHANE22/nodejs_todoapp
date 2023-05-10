d06c56d8d114bfbf5bf1095acc94d9aaa7cd2783
DEEPGULHANE22 <gulhanedeep22@gmail.com>
2023-05-10 12:14:44 +0530

    last commit

--- controllers/user.js
+++ controllers/user.js
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
 export const logout = async (req, res,next) => {
     try {
         res.status(200).cookie("token", "", { 
-            expires: new Date(Date.now),
+            expires: new Date(Date.now()),
             sameSite: process.env.NODE_ENV==="Development"?"lax":"none",
             secure: process.env.NODE_ENV==="Development"?false:true,
         })
